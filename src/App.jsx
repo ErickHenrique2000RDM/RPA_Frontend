c040fced7bab2789ac6e2f840983336586ee628b
Erick Henrique <erick.henrique@aluno.ufabc.edu.br>
2021-07-28 14:01:28 -0300

    Atualização

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+import './App.css';
+import axios from 'axios'
+import MenuLoja from './lojas/MenuLoja'
+var _ = require('lodash');
+
+const baseURL = 'http://localhost:8000/'
+
+function App() {
+
+  const [estado, setEstado] = useState({listas: []});
+  const [pesquisa, setPesquisa] = useState('');
+  const [pesquisando, setPesquisando] = useState(false);
+  const [status, setStatus] = useState('Nada pesquisado')
+
+  var num = 1;
+
+  const modificaStatus = () => {
+    num = ((num % 3) + 1);
+    setStatus(`Pesquisando${_.repeat('.', num)}`)
+  }
+
+//   document.addEventListener('keypress', function (event) {
+//     if (event.keyCode !== 13 || pesquisando) return;
+//     setPesquisando(true)
+//     pegar();
+// });
+
+  const pegar = () => {
+    setEstado({listas: []})
+    if(pesquisa.replace(/\ /g, '') === ''){
+      setStatus('Insira uma pesquisa');
+      return;
+    }
+
+    const interval = setInterval(function() {
+      modificaStatus();
+    }, 500);
+   
+    setPesquisando(true);
+    axios.get(baseURL + pesquisa).then(resp => {
+      setEstado(resp.data)
+      setPesquisando(false);
+      clearInterval(interval);
+    }).catch(err => {
+      setStatus('Erro ao pesquisar, tente novamente');
+      setPesquisando(false);
+      clearInterval(interval);
+    })
+  } 
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Ferramenta de pesquisa</h1>
+        <h3>Criado por: <a className='LinkGit' href='https://github.com/ErickHenrique2000RDM/RPA_Backend' target="_blank"><strong>Erick Henrique Silva</strong></a></h3>
+      </div>
+      <div  className='caixaPesquisa'>
+        <input className='input' placeholder="O que deseja buscar?" type="text" value={pesquisa} onChange={e => setPesquisa(e.target.value)}></input>
+        <button id='submit' className='pesquisa' onClick={() => pegar()}>Pesquisar</button>
+      </div>
+
+      <div>
+        {estado.listas.length > 0 ?
+          <div className='grid'>
+            <MenuLoja lista={estado.listas[0]} loja='Kabum'></MenuLoja>
+            <MenuLoja lista={estado.listas[1]} loja='Pichau'></MenuLoja>
+            <MenuLoja lista={estado.listas[2]} loja='Terabyte'></MenuLoja>
+          </div>
+          : <div className='status'>{status}</div>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lojas/MenuLoja.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './style.css';
+
+export default props => {
+
+    const [visible, setVisible] = useState(true);
+
+    // if(document.getElementById('Click').clicked == true){
+    //     setVisible(!visible); <textarea>
+    // }
+
+    return (
+        <div className={`MenuLoja ${visible ? '' : 'hidden'}`}>
+            <p className='loja' id='Click' onClick={ () => {
+                console.log('click');
+                setVisible(!visible);
+            }}>{props.loja}</p>
+            {/*visible*/ true ? <div>
+                {props.lista.map((item, key) => {
+                    return (
+                        <div className={`capsula ${visible ? '' : 'hidden'}`} id={key}>
+                          <p>Nome: {item.nome}</p>  
+                          <p>Preco: {item.preco}</p>  
+                          <a className='LinkItem' href={item.link} target="_blank">Acessar site</a>  
+                        </div>
+                      )
+                })}
+            </div> : <></>}
+        </div>
+    )
+    
+}
